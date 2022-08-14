b83210721ccef99cd804e699b59f96a1f763a2d3
Sergey Zhirnov <mahorishe73@gmail.com>
2022-08-14 20:16:38 +0300

    Data add in massive objects

--- src/componets/Dialogs/dialogs.jsx
+++ src/componets/Dialogs/dialogs.jsx
@@ -1,30 +1,47 @@
 import React from "react";
 import {NavLink} from 'react-router-dom';
 import s from './dialogs.module.css';
+
+const ContactItem = (props) => {
+  let path = "/dialogs/" + props.id;
+  return (
+    <div className={s.contactItem}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+
+  );
+}
+
+const MessagesItem = (props) => {
+  return (
+    <div className={s.message}>
+    {props.message}
+  </div>
+  );
+}
+
+let DialogsData = [
+  {id: 1, name: "Fred"},
+  {id: 2, name: "Jhon"},
+  {id: 3, name: "Steve"},
+]
+
+let MessagesData = [
+  {message: "Hello! My Friend"},
+  {message: "How are you?"}
+]
+
 const Dialogs = () => {
     return (
       <div className={s.wrapperDialogs}>
         <div className={s.contactDialogs}>
-          <div className={s.contactItem}>
-            <NavLink to="/dialogs/1">Ilatovskii</NavLink>
-          </div>
-          <div className={s.contactItem}>
-            <NavLink to="/dialogs/2">KAlich</NavLink>
-          </div>
-          <div className={s.contactItem}>
-            <NavLink to="/dialogs/3">Bober</NavLink>
-          </div>
-          <div className={s.contactItem}>
-            <NavLink to="/dialogs/4">Ostrov</NavLink>
-          </div>
-          <div className={s.contactItem}>
-            <NavLink to="/dialogs/5">Guwer</NavLink>
-          </div>
+          <ContactItem name={DialogsData[0].name} id={DialogsData[0].id}></ContactItem>
+          <ContactItem name={DialogsData[1].name} id={DialogsData[1].id}></ContactItem>
+          <ContactItem name={DialogsData[2].name} id={DialogsData[2].id}></ContactItem>
         </div>
         <div className={s.messagesDialogs}>
-          <div className={s.message}>
-            Hello, My Friend!!!
-          </div>
+          <MessagesItem message={MessagesData[0].message}></MessagesItem>
+          <MessagesItem message={MessagesData[1].message}></MessagesItem>
         </div>
       </div>
 
